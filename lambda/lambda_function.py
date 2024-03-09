5a432a9ebccec9449285c8cb8a79e4ea71e90e3e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-03-09 03:06:40 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -40,7 +40,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Ciao, mio piccolo nerd preferito, dimmi pure, che ti serve? Di 'anime up comandi' per la lista comandi"
+        speak_output = "Ciao, mio piccolo nerd preferito, che vuoi sapere?"
 
         return (
             handler_input.response_builder
