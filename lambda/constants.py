dc75b27ead4d12376da46870b37b0ea0faa4fe08
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-03-09 02:42:44 +0000

    updated lambda/constants.py.

--- lambda/constants.py
+++ lambda/constants.py
@@ -2,36 +2,33 @@ FALLBACK_ASK = "eh mo, ti serve altro?"
 
 HIRING_ANIME = [
     {
-        "tsukimichi - moonlit fantasy": {
-            "descr": "Makoto Misumi era solo un adolescente medio che all'improvviso fu convocato in un altro mondo come un eroe. Ma la dea di questo mondo lo definì brutto e gli tolse il suo status di eroe costringendolo a rimanere li in disparte ai bordi di quel mondo. Cosi' lui decide di vivere la sua esperienza al 100%. Questa e' la storia.",
-            "genere": "Isekai",
-            "hiring_day": "Mon",
-            "season": "2",
-            "episode": "8"
-        }
+        "name": "tsukimichi - moonlit fantasy",
+        "descr": "Makoto Misumi era solo un adolescente medio che all'improvviso fu convocato in un altro mondo come un eroe. Ma la dea di questo mondo lo definì brutto e gli tolse il suo status di eroe costringendolo a rimanere li in disparte ai bordi di quel mondo. Cosi' lui decide di vivere la sua esperienza al 100%. Questa e' la storia.",
+        "genere": "Isekai",
+        "hiring_day": "Mon",
+        "season": "2",
+        "episode": "8"
+    },
+    {
+        "name": "classroom of the elite",
+        "descr": "Ayanokoji Kiyotaka sta per cominciare il suo percorso scolastico presso il 'Liceo per la formazione di alto livello di Tokyo'. Agli studenti di tale scuola, alla fine dei corsi, è garantita l'ammissione al college scelto o verrà trovato lavoro. Si accorge subito che non e' un normale liceo e che la sua non e' una classe qualunque.",
+        "genere": "Gakuen",
+        "hiring_day": "Wed",
+        "season": "3",
+        "episode": "9"
         
     },
     {
-        "classroom of the elite": {
-            "descr": "Ayanokoji Kiyotaka sta per cominciare il suo percorso scolastico presso il 'Liceo per la formazione di alto livello di Tokyo'. Agli studenti di tale scuola, alla fine dei corsi, è garantita l'ammissione al college scelto o verrà trovato lavoro. Si accorge subito che non e' un normale liceo e che la sua non e' una classe qualunque.",
-            "genere": "Gakuen",
-            "hiring_day": "Wed",
-            "season": "3",
-            "episode": "9"
-        }
-        
+        "name": "metallic rouge",
+        "descr": "In un mondo in cui gli umani coesistono con gli androidi chiamati Nean, un gruppo noto come Immortal Nine si ribella alla società. Incaricati di eliminare i rivoltosi, un Nean di nome Rouge Redstar (alias Metal Rouge) e l'investigatrice Naomi Orthmann si recano su Marte per rintracciarli.",
+        "genere": "Cyber Punk",
+        "hiring_day": "Wed",
+        "season": "1",
+        "episode": "8"
     },
     {
-        "metallic rouge": {
-            "descr": "In un mondo in cui gli umani coesistono con gli androidi chiamati Nean, un gruppo noto come Immortal Nine si ribella alla società. Incaricati di eliminare i rivoltosi, un Nean di nome Rouge Redstar (alias Metal Rouge) e l'investigatrice Naomi Orthmann si recano su Marte per rintracciarli.",
-            "genere": "Cyber Punk",
-            "hiring_day": "Wed",
-            "season": "1",
-            "episode": "8"
-        }
-    },
-    {
-        "the witch and the beast": {
+        {
+            "name": "the witch and the beast"
             "descr": "Guideau è una giovane donna maledetta da una strega, costretta a portare con sé un oscuro segreto. Ashaf è un uomo alto con una bara legata alla schiena e segreti non ancora svelati. In un dark fantasy pieno di avventura, riusciranno gli improbabili eroi a trovare la vendetta contro le streghe?",
             "genere": "Dark Fantasy",
             "hiring_day": "Thu",
@@ -41,7 +38,8 @@ HIRING_ANIME = [
         
     },
     {
-        "frieren - oltre la fine del viaggio": {
+        {
+            "name": "frieren - oltre la fine del viaggio"
             "descr": "Frieren è una maga che fa parte della squadra di eroi che ha sconfitto il re dei demoni. Essendo un'elfa, per lei i dieci anni passati con gli altri eroi rappresentano soltanto un piccolo frammento della sua lunga vita. Ora, Frieren riflette sul significato di quelle memorie e sui vari sentimenti che prova a riguardo.",
             "genere": "Fantasy Drama Adventure",
             "hiring_day": "Fri",
@@ -50,7 +48,8 @@ HIRING_ANIME = [
         }
     },
     {
-        "firefighters daigo ": {
+        {
+            "name": "firefighters daigo ",
             "descr": "",
             "genere": "Drama Adventure",
             "hiring_day": "Sat",
@@ -59,7 +58,8 @@ HIRING_ANIME = [
         }
     },
     {
-        "blue exorcist": {
+        {
+            "name": "blue exorcist",
             "descr": "",
             "genere": "Fantasy Adventure",
             "hiring_day": "Sat",
@@ -68,7 +68,8 @@ HIRING_ANIME = [
         }
     },
     {
-        "mashle - magic and muscles": {
+        {
+            "name": "mashle - magic and muscles",
             "descr": "La storia si svolge in un mondo in cui esiste la magia e viene usata per qualunque occorrenza della vita quotidiana. Mash è un giovane che vive nascosto nella foresta e trascorre le giornate impegnandosi in potenti esercizi per il corpo. Non riesce a usare la magia ma vive comunque una vita pacifica con suo padre. Un giorno però la sua vita viene messa in pericolo; E poi guardatelo.",
             "genere": "Fantasy Adventure",
             "hiring_day": "Sat",
@@ -77,7 +78,8 @@ HIRING_ANIME = [
         }
     },
     {
-        "solo leveling": {
+        {
+            "name": "solo leveling",
             "descr": "Dopo essere stato brutalmente massacrato dai mostri in un dungeon di alto rango, Jinwoo è tornato con il Sistema, un programma che solo lui poteva vedere, che lo sta facendo salire di livello in ogni modo. Ora, è ispirato a scoprire i segreti dietro i suoi poteri e il dungeon che li ha generati.",
             "genere": "Fantasy Adventure",
             "hiring_day": "Sat",
@@ -86,7 +88,8 @@ HIRING_ANIME = [
         }
     },
     {
-        "one piece": {
+        {
+            "name": "one piece"
             "descr": "Monkey D. Rufy vuole diventare il Re dei Pirati, e non si lascerà fermare da niente e da nessuno! La rotta è segnata, le acque insidiose della Rotta Maggiore lo aspettano, e lui non si fermerà finché non avrà trovato il più grande tesoro al mondo, il leggendario One Piece!",
             "genere": "Fantasy Adventure",
             "hiring_day": "Sun",
@@ -95,7 +98,8 @@ HIRING_ANIME = [
         }           
     },
     {
-        "the fire hunter": {
+        {
+            "name": "the fire hunter"
             "descr": "Al di fuori delle barriere magiche si trova un mondo invaso da bestie infuocate note come Demoni di Fuoco, e gli unici che possono proteggere l'umanità sono i Cacciatori di Fuoco. Nei boschi oscuri dove si aggirano le bestie, Toko, un giovane abitante del villaggio, viene salvato dall'attacco di uno di questi abili inseguitori, Koshi. Ma il loro incontro non è stato casuale e un nuovo destino ha inizio.",
             "genere": "Drama Fantasy Adventure",
             "hiring_day": "Sun",
@@ -104,7 +108,8 @@ HIRING_ANIME = [
         }
     },
     {
-        "shagiri la frontier": {
+        {
+            "name": "shagiri la frontier"
             "descr": "Rakuro Hizutome è un cacciatore di giochi brutti, che gioca con il soprannome di Sunraku fino a completarli al 100%. Dopo aver completato l'ennesimo titolo, per staccare un po' la spina si fa consigliare Shangri-La Frontier, un gioco VR full-dive di prima categoria con 30 milioni di giocatori registrati. Decide quindi di mettere alla prova tutte le abilità acquisite come esperto cacciatore di kusoge all'interno di un vero e proprio gioco.",
             "genere": "Thriller Fantasy Adventure",
             "hiring_day": "Sun",
@@ -114,7 +119,8 @@ HIRING_ANIME = [
         
     },
     {
-        "theatre of darkness": {
+        {
+            "name": "theatre of darkness"
             "descr": "",
             "genere": "Dark Thriller",
             "hiring_day": "Sun",
