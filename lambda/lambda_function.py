c04182671b72eda380b44420de664bec74f893e5
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-03-09 02:48:20 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -58,15 +58,15 @@ class TodayAnimeIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         current_day = get_current_day()
-        today_list = [ anime['name'] for anime in constants.HIRING_ANIME if anime['hiring_day'] == current_day ]
-        speak_output = f"Oggi, ci sono in programma le uscite di: {', '.join(today_list) or 'stograncasso'}"
+        # today_list = [ anime['name'] for anime in constants.HIRING_ANIME if anime['hiring_day'] == current_day ]
+        # speak_output = f"Oggi, ci sono in programma le uscite di: {', '.join(today_list) or 'stograncasso'}"
 
-        # speak_output = f"Oggi, ci sono in programma le uscite di: stograncasso"
+        speak_output = f"Oggi, {current_day}, ci sono in programma le uscite di: stograncasso"
         
         return (
             handler_input.response_builder
                 .speak(speak_output)
-                .ask(constants.FALLBACK_ASK)
+                # .ask(constants.FALLBACK_ASK)
                 .response
         )
 
