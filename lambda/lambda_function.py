83a49aae54308d18540c05347a673c4c8666644e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-03-09 02:31:51 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -14,19 +14,19 @@ from ask_sdk_core.handler_input import HandlerInput
 
 from ask_sdk_model import Response
 
-from datetime import datetime
+# from datetime import datetime
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-def retrieve_day():
-    # Get the current date and time
-    current_datetime = datetime.now()
-    # Get the day of the week as an integer (Monday is 0 and Sunday is 6)
-    day_of_week = current_datetime.weekday()
-    # Get the day of the week as a string (e.g., 'Mon', 'Tue', etc.)
-    day_of_week_str = current_datetime.strftime('%a')
-    return day_of_week_str
+# def retrieve_day():
+#     # Get the current date and time
+#     current_datetime = datetime.now()
+#     # Get the day of the week as an integer (Monday is 0 and Sunday is 6)
+#     day_of_week = current_datetime.weekday()
+#     # Get the day of the week as a string (e.g., 'Mon', 'Tue', etc.)
+#     day_of_week_str = current_datetime.strftime('%a')
+#     return day_of_week_str
 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
