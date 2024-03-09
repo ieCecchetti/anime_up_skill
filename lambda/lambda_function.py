2bec1cfca671b000869dc8339ae7a40c22bfc101
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-03-09 02:04:20 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -52,7 +52,7 @@ class TodayAnimeIntentHandler(AbstractRequestHandler):
     """Handler for Hello World Intent."""
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("HelloWorldIntent")(handler_input)
+        return ask_utils.is_intent_name("TodayAnimeIntent")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
