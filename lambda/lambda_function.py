3bbd217382c1e3548275db7ddc88f798ea4b88b7
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-03-09 02:19:01 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -38,13 +38,16 @@ class LaunchRequestHandler(AbstractRequestHandler):
                 .response
         )
 
-def get_current_day():
+def retrieve_day():
+    # Get the current date and time
     current_datetime = datetime.now()
+    # Get the day of the week as an integer (Monday is 0 and Sunday is 6)
     day_of_week = current_datetime.weekday()
     # Get the day of the week as a string (e.g., 'Mon', 'Tue', etc.)
     day_of_week_str = current_datetime.strftime('%a')
     return day_of_week_str
-
+    
+    
 class TodayAnimeIntentHandler(AbstractRequestHandler):
     """Handler for TodayAnimeIntent."""
     def can_handle(self, handler_input):
