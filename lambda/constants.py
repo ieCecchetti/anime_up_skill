50ce5830c168075c524d593203d2555faa084ce3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-03-09 01:36:41 +0000

    updated lambda/constants.py.

---
+++ lambda/constants.py
@@ -0,0 +1,39 @@
+HIRING_ANIME = {
+    "tsukimichi - moonlit fantasy": {
+        "descr": "Makoto Misumi era solo un adolescente medio che all'improvviso fu convocato in un altro mondo come un eroe. Ma la dea di questo mondo lo definì brutto e gli tolse il suo status di eroe costringendolo a rimanere li in disparte ai bordi di quel mondo. Cosi' lui decide di vivere la sua esperienza al 100%. Questa e' la storia.",
+        "genere": "Isekai",
+        "hiring_day": "Mon",
+        "last_ep": "8"
+    },
+    "classroom of the elite": {
+        "descr": "Ayanokoji Kiyotaka sta per cominciare il suo percorso scolastico presso il 'Liceo per la formazione di alto livello di Tokyo'. Agli studenti di tale scuola, alla fine dei corsi, è garantita l'ammissione al college scelto o verrà trovato lavoro. Si accorge subito che non e' un normale liceo e che la sua non e' una classe qualunque.",
+        "genere": "Gakuen",
+        "hiring_day": "Wed",
+        "last_ep": "9",
+    },
+    "metallic rouge": {
+        "descr": "In un mondo in cui gli umani coesistono con gli androidi chiamati Nean, un gruppo noto come Immortal Nine si ribella alla società. Incaricati di eliminare i rivoltosi, un Nean di nome Rouge Redstar (alias Metal Rouge) e l'investigatrice Naomi Orthmann si recano su Marte per rintracciarli.",
+        "genere": "Cyber Punk",
+        "hiring_day": "Wed",
+        "last_ep": "8",
+    },
+    "the witch and the beast": {
+        "descr": "Guideau è una giovane donna maledetta da una strega, costretta a portare con sé un oscuro segreto. Ashaf è un uomo alto con una bara legata alla schiena e segreti non ancora svelati. In un dark fantasy pieno di avventura, riusciranno gli improbabili eroi a trovare la vendetta contro le streghe?",
+        "genere": "Dark Fantasy",
+        "hiring_day": "Thu",
+        "last_ep": "8",
+    },
+    "frieren - oltre la fine del viaggio": {
+        "descr": "Frieren è una maga che fa parte della squadra di eroi che ha sconfitto il re dei demoni. Essendo un'elfa, per lei i dieci anni passati con gli altri eroi rappresentano soltanto un piccolo frammento della sua lunga vita. Ora, Frieren riflette sul significato di quelle memorie e sui vari sentimenti che prova a riguardo.",
+        "genere": "Fantasy Drama Adventure",
+        "hiring_day": "Fri",
+        "last_ep": "26",
+    },
+    "mashle - magic and muscles": {
+        "descr": "La storia si svolge in un mondo in cui esiste la magia e viene usata per qualunque occorrenza della vita quotidiana. Mash è un giovane che vive nascosto nella foresta e trascorre le giornate impegnandosi in potenti esercizi per il corpo. Non riesce a usare la magia ma vive comunque una vita pacifica con suo padre. Un giorno però la sua vita viene messa in pericolo; E poi guardatelo.",
+        "genere": "Fantasy Adventure",
+        "hiring_day": "Fri",
+        "last_ep": "12",
+    }
+    
+}
