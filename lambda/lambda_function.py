14fa64d21b60a7f4d2f371d14e9ae324b0ed2270
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-03-21 10:13:14 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -180,7 +180,7 @@ class LastEpisodeIntentHandler(AbstractRequestHandler):
             selected_anime = utils.get_closer_name(anime_name, anime_list)
             anime_info = utils.get_info_from_anime(selected_anime)
             if anime_info:
-                speak_output = f"L'ultimo episodio di {anime_name} e' {anime_info['episode']}"
+                speak_output = f"L'ultimo episodio di {anime_name} è {anime_info['episode']}"
             else:
                 speak_output = f"Scusa ma non ho trovato nessuna informazione associata a: {anime_name}. Prova a scandire meglio il nome."
         
@@ -201,16 +201,22 @@ class WhenOutAnimeIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         # get the handler_input["anime_name"] param
-        anime_name = handler_input.request_envelope.request.intent.slots["anime_name"].value
-        anime_list = [anime["name"] for anime in constants.AIRING_ANIME]
-        selected_anime = utils.get_closer_name(anime_name, anime_list)
-        anime_info = utils.get_info_from_anime(selected_anime)
-        day_of_week = constants.DAY_OF_THE_WEEK[anime_info['airing_day']]
-        if anime_info:
-            # day_of_the_week = constants.DAY_OF_THE_WEEK[day_of_week_short]
-            speak_output = f"{anime_name} esce tutti i {day_of_week}"
+        # Initialize session attributes
+        session_attr = handler_input.attributes_manager.session_attributes
+        # get the handler_input["anime_name"] param
+        anime_name = handler_input.request_envelope.request.intent.slots["anime_name"].value or session_attr.get('selected_anime', None)
+        if not anime_name:
+            speak_output = "Non ho capito di che anime stiamo parlando!"
         else:
-            speak_output = f"Scusa ma non ho trovato nessuna informazione associata a: {anime_name}. Prova a scandire meglio il nome."
+            anime_list = [anime["name"] for anime in constants.AIRING_ANIME]
+            selected_anime = utils.get_closer_name(anime_name, anime_list)
+            anime_info = utils.get_info_from_anime(selected_anime)
+            day_of_week = constants.DAY_OF_THE_WEEK[anime_info['airing_day']]
+            if anime_info:
+                # day_of_the_week = constants.DAY_OF_THE_WEEK[day_of_week_short]
+                speak_output = f"{anime_name} esce tutti i {day_of_week}"
+            else:
+                speak_output = f"Scusa ma non ho trovato nessuna informazione associata a: {anime_name}. Prova a scandire meglio il nome."
         
         return (
             handler_input.response_builder
